41942542b0f9268f5b692b5720aff0f51627a8ca
Mhmd-S <65135074+Mhmd-S@users.noreply.github.com>
2022-11-17 17:43:39 +0800

    Updates

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen, act } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
